32307acf2c3965dfb2d985c5e1f0c5e41888a8cf
MD JAHIDUL ALAM MOJUMDER <asmjahid@users.noreply.github.com>
2018-05-16 00:35:44 +0600

    New features added

--- app/src/main/java/com/asmjahid/gridlayoutdesgin/ActivityOne.java
+++ app/src/main/java/com/asmjahid/gridlayoutdesgin/ActivityOne.java
@@ -2,9 +2,18 @@ package com.asmjahid.gridlayoutdesgin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityOne extends AppCompatActivity {
+
+    private Spinner spinner;
+    private static final String[]paths = {"Books", "Restaurant", "Customs","Airport"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,63 @@ public class ActivityOne extends AppCompatActivity {
             String info = getIntent().getStringExtra("info");
             txtInfo.setText(info);
         }
+
+        Spinner staticSpinner = (Spinner) findViewById(R.id.static_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.list,
+                        android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        staticSpinner.setAdapter(staticAdapter);
+
+        Spinner dynamicSpinner = (Spinner) findViewById(R.id.dynamic_spinner);
+
+        String[] items = new String[] { "Chai Latte", "Green Tea", "Black Tea" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+
+        dynamicSpinner.setAdapter(adapter);
+
+        dynamicSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                switch (position) {
+                    case 0:
+
+                        Toast.makeText(ActivityOne.this, "State :"+ (String) parent.getItemAtPosition(position),
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 1:
+
+                        Toast.makeText(ActivityOne.this, "State :"+ (String) parent.getItemAtPosition(position),
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+
+                        Toast.makeText(ActivityOne.this, "State :"+ (String) parent.getItemAtPosition(position),
+                                Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 }
